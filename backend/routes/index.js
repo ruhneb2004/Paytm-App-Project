749111ddc709063eef8b5a121793278158102128
ruhneb2004 <146971477+ruhneb2004@users.noreply.github.com>
2024-01-26 20:10:08 +0530

    backend update

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,10 +1,10 @@
 const express = require("express");
-const app = express();
+
+const router = express.Router();
 
 const userRouter = require("./user");
 const accountRouter = require("./account");
-app.use("/user", userRouter);
-app.use("/account", accountRouter);
-const router = express.Router();
+router.use("/user", userRouter);
+router.use("/account", accountRouter);
 
 module.exports = router;
